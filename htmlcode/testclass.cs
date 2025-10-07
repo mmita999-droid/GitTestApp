50c6dc6b0b8fb318f22b0ba5e9f86d1507780118
mmita999-droid <mmita999@gmail.com>
2025-10-07 13:04:46 +0530

    7 oct

---
+++ htmlcode/testclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
