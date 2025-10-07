07772b0475276dbb8b6579d1d5d0fca19b9e9be6
mmita999-droid <mmita999@gmail.com>
2025-10-07 13:05:48 +0530

    Update testclass.cs

--- htmlcode/testclass.cs
+++ htmlcode/testclass.cs
@@ -6,7 +6,7 @@ public class Class1
     {
         static void Main()
         {
-            for (int i = 1; i <= 10; i++)
+            for (int i = 1; i <= 20; i++)
             {
                 Console.WriteLine(i);
             }
